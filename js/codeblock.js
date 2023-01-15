edf6b18d4bd7723547048f461d45e8a308a0a4e4
profezzorn <hubbe@hubbe.net>
2023-01-15 05:04:35 -0600

    fix copybutton

--- js/codeblock.js
+++ js/codeblock.js
@@ -2,23 +2,19 @@ var codeBlocks = document.querySelectorAll('pre.highlight');
 
 codeBlocks.forEach(function (codeBlock) {
   var  Button = document.createElement('button');
-   Button.className = ' ';
-   Button.type = 'button';
-   Button.ariaLabel = 'Copy code to clipboard';
-   Button.innerText = 'Copy';
+  Button.className = 'copy';
+  Button.type = 'button';
+  Button.ariaLabel = 'Copy code to clipboard';
+  Button.innerText = 'Copy';
 
+  codeBlock.append(Button);
 
-  codeBlock.append( Button);
-
-
-   Button.addEventListener('click', function () {
+  Button.addEventListener('click', function () {
     var code = codeBlock.querySelector('code').innerText.trim();
     window.navigator.clipboard.writeText(code);
 
-
-     Button.innerText = 'Copied';
+    Button.innerText = 'Copied';
     var fourSeconds = 4000;
-
 
     setTimeout(function () {
        Button.innerText = 'Copy';
